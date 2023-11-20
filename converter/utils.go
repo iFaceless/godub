b2254e7d891a5d57f0356e6ed3f7976d98def518
Yahya Ayash <yahya31415@gmail.com>
2023-11-20 07:31:43 +0530

    multiple os support

--- converter/utils.go
+++ converter/utils.go
@@ -2,6 +2,7 @@ package converter
 
 import (
 	"fmt"
+	"runtime"
 
 	"os/exec"
 )
@@ -15,9 +16,21 @@ func GetEncoderName() string {
 }
 
 func IsCommandAvailable(name string) bool {
-	cmd := exec.Command("where", name)
+	cmd := exec.Command(getCheckCommand(), name)
 	if err := cmd.Run(); err != nil {
 		return false
 	}
 	return true
+}
+
+func getOS() string {
+	return runtime.GOOS
+}
+
+func getCheckCommand() string {
+	if getOS() == "windows" {
+		return "where"
+	} else {
+		return "which"
+	}
 }
